dbe7575d7d45d7a1a207adfc15b644ea98cf0a0f
ceerik <asdert458@gmail.com>
2020-05-10 20:37:31 +0200

    Update TodoItemController.java

--- src/main/java/com/TodoMVC/TodoItemController.java
+++ src/main/java/com/TodoMVC/TodoItemController.java
@@ -3,6 +3,7 @@ package com.TodoMVC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class TodoItemController {
 	@Autowired
